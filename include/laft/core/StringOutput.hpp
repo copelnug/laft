73db4b45a629e10b20f9561a688c4538e704db03
François Godin <copelnug@gmail.com>
2014-10-20 21:55:55 -0400

    Implemented laft::core::StringOutput. This class will be used to build string with the same operations used to write to a file.

---
+++ include/laft/core/StringOutput.hpp
@@ -0,0 +1,26 @@
+#ifndef LAFT_CORE_STRING_OUTPUT_HPP_INCLUDED
+#define LAFT_CORE_STRING_OUTPUT_HPP_INCLUDED
+
+#include <laft/core/Output.hpp>
+namespace laft
+{
+	namespace core
+	{
+		/**
+			\brief Utility to build string.
+		*/
+		class StringOutput : public Output<StringOutput>
+		{
+			public:
+				void write(char const *value, size_t length);
+				
+				void clear();
+				
+				std::string extract();
+				std::string const& get() const;
+			private:
+				std::string value_; /**< \brief String buffer. */
+		};
+	}
+}
+#endif
---
+++ src/core/StringOutput.cpp
@@ -0,0 +1,38 @@
+#include <laft/core/StringOutput.hpp>
+
+/**
+	\brief Write bytes to the string.
+	\param value Bytes to write.
+	\param length Number of bytes to write.
+*/
+void laft::core::StringOutput::write(char const *value, size_t length)
+{
+	value_.append(value, length);
+}
+/**
+	\brief Clear the content of the string.
+*/
+void laft::core::StringOutput::clear()
+{
+	value_.erase();
+}
+/**
+	\brief Extract the content of the string. Clearing it in the process.
+	\return Value that was in the string.
+*/
+std::string laft::core::StringOutput::extract()
+{
+	std::string newVal;
+	std::swap(value_, newVal);
+	return newVal;
+}
+/**
+	\brief Get the current content of the string.
+	\return Current content of the string.
+	
+	When modifying the StringOutput, the value pointed by the reference may change.
+*/
+std::string const& laft::core::StringOutput::get()  const
+{
+	return value_;
+}
