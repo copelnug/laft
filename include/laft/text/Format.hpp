d9d8a6ed23fdd965b37b4a3e7609613fc422a2de
François Godin <copelnug@gmail.com>
2015-06-27 14:00:21 -0400

    Implemented arguments in laft::text::Formatter method. The text $NUMBER is now replaced by the corresponding argument.

--- include/laft/text/Format.hpp
+++ include/laft/text/Format.hpp
@@ -1,5 +1,6 @@
 #ifndef LAFT_TEXT_FORMAT_HPP_INCLUDED
 #define LAFT_TEXT_FORMAT_HPP_INCLUDED
+#include <stdexcept>
 #include <string>
 
 #include <laft/core/Mixin.hpp>
@@ -24,6 +25,8 @@ namespace laft
 				TextOutputFormatter(Output& output);
 			
 				void write(char const* text, size_t length);
+				template <typename T>
+				void write(T const& value);
 			private:
 				Output& output_;
 		};
@@ -43,11 +46,111 @@ namespace laft
 {
 	namespace text
 	{
+		namespace impl
+		{
+			// TODO: Replace Form with a functor to allow reuse of ArgsHelpers. Or give the functor to handle. This allow to reuse a same ArgsHelper for multiples functions
+			template <typename Form, typename ...T>
+			class ArgsHelpers;
+			
+			template <typename Form, typename T, typename ...R>
+			class ArgsHelpers<Form, T, R...> : public ArgsHelpers<Form, R...>
+			{
+				public:
+					ArgsHelpers(Form& formatter, T const& arg, R const& ... rest) :
+						ArgsHelpers<Form, R...>(formatter, rest...),
+						arg_(arg)
+					{}
+					void handle(size_t i)
+					{
+						if(i == 0)
+							ArgsHelpers<Form>::formatter_.write(arg_);
+						else
+							ArgsHelpers<Form, R...>::handle(i - 1);
+					}
+				private:
+					T const& arg_;
+			};
+			template <typename Form>
+			class ArgsHelpers<Form>
+			{
+				public:
+					ArgsHelpers(Form& formatter) :
+						formatter_(formatter)
+					{}
+					void handle(size_t)
+					{
+						// TODO: New exception?
+						throw std::out_of_range("Format args is not in valid range.");
+					}
+				protected:
+					Form& formatter_;
+			};
+		}
 		template <typename Derived>
 		template <typename ...T>
 		Formatter<Derived>& Formatter<Derived>::format(size_t length, char const* formatString, const T&... args)
 		{
-			this->self().write(formatString, length);
+			impl::ArgsHelpers<Derived, T...> helper(this->self(), args...);
+			// TODO use string::find. Do not write characters 1 by 1.
+			bool parsing = false;
+			bool found = false;
+			size_t index = 0;
+			for(size_t i = 0; i < length; ++i)
+			{
+				char c = formatString[i];
+				
+				switch(c)
+				{
+					case '$':
+						found = true;
+						if(parsing)
+						{
+							parsing = false;
+							this->self().write(&c, 1);
+						}
+						else
+						{
+							index = 0;
+							found = false;
+							parsing = true;
+						}
+						break;
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						if(parsing)
+						{
+							found = true;
+							index = index * 10 + c - '0';
+							break;
+						}
+						// Continue
+					default:
+						if(parsing)
+						{
+							if(!found)
+								throw std::logic_error("Missing value after $");
+							helper.handle(index);
+							parsing = false;
+						}
+						this->self().write(&c, 1);
+						break;
+				}
+			}
+				
+			if(parsing)
+			{
+				if(!found)
+					throw std::logic_error("Missing value after $");
+				helper.handle(index);
+			}
 			return *this;
 		}
 		template <typename Output>
@@ -59,10 +162,16 @@ namespace laft
 		{
 			output_.write(text, length);
 		}
+		template <typename Output>
+		template <typename T>
+		void TextOutputFormatter<Output>::write(T const& value)
+		{
+			output_.write(value);
+		}
 		template <typename ...T>
 		std::string format(std::string const& formatString, const T&... args)
 		{
-			return format(formatString.length(), formatString.c_str());
+			return format(formatString.length(), formatString.c_str(), args...);
 		}
 		template <typename ...T>
 		std::string format(size_t length, char const* formatString, const T&... args)
--- tests/text/Format.cpp
+++ tests/text/Format.cpp
@@ -1,11 +1,30 @@
 #include "../catch.hpp"
 #include "laft/text/Format.hpp"
 
+TEST_CASE("format_nothing", "[text]")
+{
+	std::string basic{"basic"};
+	REQUIRE(laft::text::format(basic) == basic);
+
+	std::string empty{""};
+	REQUIRE(laft::text::format(empty) == empty);
+	
+	std::string numbers{"a0a1a2a3a4a5a6a7a8a9a"};
+	REQUIRE(laft::text::format(numbers) == numbers);
+}
 TEST_CASE("format_basic", "[text]")
 {
-	std::string BASIC = "basic";
-	REQUIRE(laft::text::format(BASIC) == BASIC);
-
-	std::string EMPTY = "";
-	REQUIRE(laft::text::format(EMPTY) == EMPTY);
+	REQUIRE(laft::text::format("This is test #$0.", "0") == "This is test #0.");
+	REQUIRE(laft::text::format("#$0 come after #$1.", "1", "0") == "#1 come after #0.");
+	REQUIRE(laft::text::format("This $0 is longer.", "value") == "This value is longer.");
+	REQUIRE(laft::text::format("Lets $0 and $0.", "repeat") == "Lets repeat and repeat.");
+	REQUIRE(laft::text::format("$0 with a replacement.", "Start") == "Start with a replacement.");
+	REQUIRE(laft::text::format("Replacement at the $0", "end.") == "Replacement at the end.");
+	REQUIRE(laft::text::format("$0", "Only.") == "Only.");
+}
+TEST_CASE("format_escape", "[text]")
+{
+	REQUIRE(laft::text::format("This is worth 10$$.") == "This is worth 10$.");
+	REQUIRE(laft::text::format("Symbol followed by a variable: $$$0.", "var") == "Symbol followed by a variable: $var.");
+	REQUIRE(laft::text::format("Symbol $0 can be in replacement without escaping.", "$") == "Symbol $ can be in replacement without escaping.");
 }
